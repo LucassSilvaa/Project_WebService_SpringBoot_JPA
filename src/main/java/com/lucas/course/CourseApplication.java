4ca668481a7acf9582c5bbd8dfa1687c570dccd9
Lucas <contato.lucassilva28@gmail.com>
2025-03-28 14:27:07 -0300

    update1

--- src/main/java/com/lucas/course/CourseApplication.java
+++ src/main/java/com/lucas/course/CourseApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//anotation - processamento por baixo dos panos no código fonte, realizada pelo SPRING
 public class CourseApplication {
 
 	public static void main(String[] args) {
