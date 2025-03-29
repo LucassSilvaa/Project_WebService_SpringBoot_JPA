edeacd65b1df464c7ba9fe48ee545e35e7299ae6
Lucas <contato.lucassilva28@gmail.com>
2025-03-29 14:31:10 -0300

    Camada de serviço e registro de componentes

--- src/main/java/com/lucas/course/repositories/UserRepository.java
+++ src/main/java/com/lucas/course/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.lucas.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.lucas.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	//como essa interface já existe, não precisa definir nada dentro dela, ele vai usar as parametrizações padrão da interface JPARepository, levando em consideração os parâmetros inseridos por nós (Objeto !User! e o tipo do ID, que é !Long!)
