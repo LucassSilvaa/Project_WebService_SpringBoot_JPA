bf98dc0fae328de683dc365974e6b3f80b452419
Lucas <contato.lucassilva28@gmail.com>
2025-03-29 09:46:00 -0300

    Banco de Dados H2, test profile, JPA

--- src/main/java/com/lucas/course/CourseApplication.java
+++ src/main/java/com/lucas/course/CourseApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//anotation - processamento por baixo dos panos no código fonte, realizada pelo SPRING
 public class CourseApplication {
 
 	public static void main(String[] args) {
